6a997d1245013a4484b09a470acf8e1e957c2cfb
masaki <m.kudou.sys24@morijyobi.ac.jp>
2025-10-07 14:52:46 +0900

    基盤変更

---
+++ demo/InputForm/index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>部門選択</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <main>
+        <div class="contents">
+            <div class="individual">
+                <h2>個人戦</h2>
+                <ul>
+                    <li><a href="./Individual/E_under4.php">小学生4年生以下</a></li>
+                    <li><a href="./Individual/E_upper5.php">小学生5年生以上</a></li>
+                    <li><a href="./Individual/J_men.php">中学生男子</a></li>
+                    <li><a href="./Individual/J_woman.php">中学生女子</a></li>
+                    <li><a href="./Individual/H_men.php">高校生男子</a></li>
+                    <li><a href="./Individual/H_women.php">高校生女子</a></li>
+                </ul>
+
+            </div>
+            <div class="team">
+                <h2>団体戦</h2>
+                <ul>
+                    <li><a href="./Team/Elementary.php">小学生団体</a></li>
+                    <li><a href="./Team/T_j_men.php">中学生男子団体</a></li>
+                    <li><a href="./Team/T_J_women.php">中学生女子団体</a></li>
+                    <li><a href="./Team/T_h_men.php">高校生男子団体</a></li>
+                    <li><a href="./Team/T_h_women.php">高校生女子団体</a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
--- demo/next.php
+++ demo/next.php
@@ -18,27 +18,13 @@
     </header>
     <main>
         <div class="contents">
-            <div class="individual">
-                <h2>個人戦</h2>
-                <ul>
-                    <li><a href="./Individual/E_under4.php">小学生4年生以下</a></li>
-                    <li><a href="./Individual/E_under4.php">小学生5年生以上</a></li>
-                    <li><a href="./Individual/E_under4.php">中学生男子</a></li>
-                    <li><a href="./Individual/E_under4.php">中学生女子</a></li>
-                    <li><a href="./Individual/E_under4.php">高校生男子</a></li>
-                    <li><a href="./Individual/E_under4.php">高校生女子</a></li>
-                </ul>
-
+            <div class="assistant">
+                <p>補助員用</p>
+                <a href="./InputForm/index.php">入力フォーム</a>
             </div>
-            <div class="team">
-                <h2>団体戦</h2>
-                <ul>
-                    <li><a href="./Individual/E_under4.php">小学生団体</a></li>
-                    <li><a href="./Individual/E_under4.php">中学生男子団体</a></li>
-                    <li><a href="./Individual/E_under4.php">中学生女子団体</a></li>
-                    <li><a href="./Individual/E_under4.php">高校生男子団体</a></li>
-                    <li><a href="./Individual/E_under4.php">高校生女子団体</a></li>
-                </ul>
+            <div class="administrater">
+                <p>管理者用</p>
+                <a href="./Administrater/index.php">参照・管理サイト</a>
             </div>
         </div>
     </main>
