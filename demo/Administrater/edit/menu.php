2748563607dba6485f2f37e3786c67481fe70375
masaki <m.kudou.sys24@morijyobi.ac.jp>
2025-11-10 15:04:33 +0900

    パスの変更

--- demo/Administrater/edit/menu.php
+++ demo/Administrater/edit/menu.php
@@ -34,7 +34,7 @@
         </div>
         
         <div class="back-link">
-            <a href="index.html" class="back-text">← 戻る</a>
+            <a href="admini_Top.php" class="back-text">← 戻る</a>
         </div>
     </div>
 </body>
