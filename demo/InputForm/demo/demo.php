71f7972719be72dbf4856e61375de75be8c7fa9a
kichinai <y.kichinai.sys24@morijyobi.ac.jp>
2025-11-10 15:01:44 +0900

    demoのtop画面作成

---
+++ demo/InputForm/demo/demo.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>デモ</title>
+</head>
+<body>
+    <h1>入力の練習</h1>
+    <h2>どちらか選択してください</h2>
+    <a>個人戦</a>
+    <a href =" ./team/team.php">団体戦</a>
+    <a href = "../index.php">戻る</a>
+</body>
+</html>
---
+++ demo/InputForm/demo/team/team.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>デモ</title>
+</head>
+<body>
+    <h1>団体戦 ○○大会 ○○部門</h1>
+    <h2>チーム名    対     チーム名</h2>
+    <a>決定</a>
+    <a href = "../demo.php">戻る</a>
+</body>
+</html>
