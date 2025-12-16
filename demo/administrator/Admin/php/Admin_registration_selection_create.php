2145ec0698f29ffd7c66868f9e81cbd74aa15909
inakawa <t.inakawa.sys24@morijyobi.ac.jp>
2025-12-16 14:38:24 +0900

    細やかな変更

--- demo/administrator/Admin/php/Admin_registration_selection_create.php
+++ demo/administrator/Admin/php/Admin_registration_selection_create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>部門作成</title>
-    <link rel="stylesheet" href="category-create-style.css">
+    <link rel="stylesheet" href="../css/Admin_registration_selection_create.css">
 </head>
 <body>
     <div class="breadcrumb">
